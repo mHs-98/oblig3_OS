36062b76894f127b13919941b28474fd996b18a5
Mahamed H Said <mahameds@stud.ntnu.no>
2020-04-05 13:34:54 +0200

    ingen lokal feilmelding

--- 12.4c.ps1
+++ 12.4c.ps1
@@ -1,24 +1,17 @@
-#min løsning paa oppgave 12.4c i opsys
+﻿#min løsning paa oppgave 12.4c i opsys
 #author: Mahamed H Said NTNU GJØVIK BITSEC
-
-
-
 foreach ($id in $args) {
-    
-    # variabel som holder info som skal skriver til fil etterhvert 
-    $info=""
-
-    $info+="*********** Minne info om prosess med PID $id ***********"
+    # variabel som holder info som skal skriver til fil etterhvert
+    $info
+    $info += "*********** Minne info om prosess med PID $id"
     #newline
     $info+="`n"
-    $info+="Virtual Memory Size: $($(Get-Process -Id $id).VM) byte(s)"
+    $info += "Virtual Memory Size: $($(Get-Process -Id $id).VM) byte(s)"
     #newline
     $info+="`n"
-    $info+="Working Set Size: $($(Get-Process -Id $id).WS) byte(s)"
-
-    # lager filnavn: mix av prosessnummer etterfulgt av ".meminfo"
-    $filNavn="$($id)_$(Get-Date -format 'yyyyMMdd').meminfo"
-
+    $info += "Working Set Size: $($(Get-Process -Id $id).WS) byte(s)"
+    # lager filnavn: mix av prosessnummer etterfulgt av ".meminfo
+    $filNavn = "$($id)_$(Get-Date -format 'yyyyMMdd').meminfo"
     # skriv til fil
     $info | Out-File $filNavn
 }
--- 13.10b.ps1
+++ 13.10b.ps1
@@ -1,4 +1,4 @@
-#min løsning til oppgave 13.10b i opsys
+﻿#min løsning til oppgave 13.10b i opsys
 #author: Mahamed H Said NTNU GJøvik Bitsec
 #faar tak i alle kjørende proseser der det inneholder "chrome" og teller traader
- Get-Process chrome | ForEach-Object { Write-Host "chrome $($_.Id) $($_.Threads.Count)" }
+ Get-Process chrome | ForEach-Object { Write-Output "chrome $($_.Id) $($_.Threads.Count)" }
